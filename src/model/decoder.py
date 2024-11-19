30c7c84571be7a1780d7cbb66b6fcfc9f802c6b8
brian tran <btran@hamilton.edu>
2024-11-19 11:11:45 -0500

    organized the code into seperate files

---
+++ src/model/decoder.py
@@ -0,0 +1,103 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from src.model.attention import BahdanauAttention
+import src.config as c
+
+
+# class DecoderRNN(nn.Module):
+#     def __init__(self, hidden_size, output_size):
+#         super(DecoderRNN, self).__init__()
+#         self.embedding = nn.Embedding(output_size, hidden_size)
+#         self.gru = nn.GRU(hidden_size, hidden_size, batch_first=True)
+#         self.out = nn.Linear(hidden_size, output_size)
+
+#     def forward(self, encoder_outputs, encoder_hidden, target_tensor=None):
+#         batch_size = encoder_outputs.size(0)
+#         decoder_input = torch.empty(batch_size, 1, dtype=torch.long).fill_(c.SOS_token)
+#         decoder_hidden = encoder_hidden
+#         decoder_outputs = []
+
+#         for i in range(c.MAX_LENGTH):
+#             decoder_output, decoder_hidden = self.forward_step(
+#                 decoder_input, decoder_hidden
+#             )
+#             decoder_outputs.append(decoder_output)
+
+#             if target_tensor is not None:
+#                 # Teacher forcing: Feed the target as the next input
+#                 decoder_input = target_tensor[:, i].unsqueeze(1)  # Teacher forcing
+#             else:
+#                 # Without teacher forcing: use its own predictions as the next input
+#                 _, topi = decoder_output.topk(1)
+#                 decoder_input = topi.squeeze(
+#                     -1
+#                 ).detach()  # detach from history as input
+
+#         decoder_outputs = torch.cat(decoder_outputs, dim=1)
+#         decoder_outputs = F.log_softmax(decoder_outputs, dim=-1)
+#         return (
+#             decoder_outputs,
+#             decoder_hidden,
+#             None,
+#         )  # We return `None` for consistency in the training loop
+
+#     def forward_step(self, input, hidden):
+#         output = self.embedding(input)
+#         output = F.relu(output)
+#         output, hidden = self.gru(output, hidden)
+#         output = self.out(output)
+#         return output, hidden
+
+
+class AttnDecoderRNN(nn.Module):
+    def __init__(self, hidden_size, output_size, dropout_p=0.1):
+        super(AttnDecoderRNN, self).__init__()
+        self.embedding = nn.Embedding(output_size, hidden_size)
+        self.attention = BahdanauAttention(hidden_size)
+        self.gru = nn.GRU(2 * hidden_size, hidden_size, batch_first=True)
+        self.out = nn.Linear(hidden_size, output_size)
+        self.dropout = nn.Dropout(dropout_p)
+
+    def forward(self, encoder_outputs, encoder_hidden, target_tensor=None):
+        batch_size = encoder_outputs.size(0)
+        decoder_input = torch.empty(batch_size, 1, dtype=torch.long).fill_(c.SOS_token)
+        decoder_hidden = encoder_hidden
+        decoder_outputs = []
+        attentions = []
+
+        for i in range(c.MAX_LENGTH):
+            decoder_output, decoder_hidden, attn_weights = self.forward_step(
+                decoder_input, decoder_hidden, encoder_outputs
+            )
+            decoder_outputs.append(decoder_output)
+            attentions.append(attn_weights)
+
+            if target_tensor is not None:
+                # Teacher forcing: Feed the target as the next input
+                decoder_input = target_tensor[:, i].unsqueeze(1)  # Teacher forcing
+            else:
+                # Without teacher forcing: use its own predictions as the next input
+                _, topi = decoder_output.topk(1)
+                decoder_input = topi.squeeze(
+                    -1
+                ).detach()  # detach from history as input
+
+        decoder_outputs = torch.cat(decoder_outputs, dim=1)
+        decoder_outputs = F.log_softmax(decoder_outputs, dim=-1)
+        attentions = torch.cat(attentions, dim=1)
+
+        return decoder_outputs, decoder_hidden, attentions
+
+    def forward_step(self, input, hidden, encoder_outputs):
+        embedded = self.dropout(self.embedding(input))
+
+        query = hidden.permute(1, 0, 2)
+        context, attn_weights = self.attention(query, encoder_outputs)
+        input_gru = torch.cat((embedded, context), dim=2)
+
+        output, hidden = self.gru(input_gru, hidden)
+        output = self.out(output)
+
+        return output, hidden, attn_weights
---
+++ src/utils.py
@@ -0,0 +1,47 @@
+import torch
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler
+
+
+import src.config as c
+
+
+def indexesFromSentence(lang, sentence):
+    return [lang.word2index[word] for word in sentence.split(" ")]
+
+
+def tensorFromSentence(lang, sentence):
+    indexes = indexesFromSentence(lang, sentence)
+    indexes.append(c.EOS_token)
+    return torch.tensor(indexes, dtype=torch.long).view(1, -1)
+
+
+def tensorsFromPair(input_lang, output_lang, pair):
+    input_tensor = tensorFromSentence(input_lang, pair[0])
+    target_tensor = tensorFromSentence(output_lang, pair[1])
+    return (input_tensor, target_tensor)
+
+
+def get_dataloader(prepareData, batch_size):
+    input_lang, output_lang, pairs = prepareData("eng", "fra", True)
+
+    n = len(pairs)
+    input_ids = np.zeros((n, c.MAX_LENGTH), dtype=np.int32)
+    target_ids = np.zeros((n, c.MAX_LENGTH), dtype=np.int32)
+
+    for idx, (inp, tgt) in enumerate(pairs):
+        inp_ids = indexesFromSentence(input_lang, inp)
+        tgt_ids = indexesFromSentence(output_lang, tgt)
+        inp_ids.append(c.EOS_token)
+        tgt_ids.append(c.EOS_token)
+        input_ids[idx, : len(inp_ids)] = inp_ids
+        target_ids[idx, : len(tgt_ids)] = tgt_ids
+
+    train_data = TensorDataset(
+        torch.LongTensor(input_ids), torch.LongTensor(target_ids)
+    )
+
+    train_sampler = RandomSampler(train_data)
+    train_dataloader = DataLoader(
+        train_data, sampler=train_sampler, batch_size=batch_size
+    )
+    return input_lang, output_lang, train_dataloader
